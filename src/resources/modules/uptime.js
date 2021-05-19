ecb24703691c37c9fd0817fbd3bb1b7ccb58f2f1
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-05-19 18:01:01 -0400

    Create uptime.js - Get bot uptime

---
+++ src/resources/modules/uptime.js
@@ -0,0 +1,10 @@
+module.exports = function(client){
+    let totalSeconds = Math.floor(client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    totalSeconds %= 86400;
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+    return (`${days}d, ${hours}h, ${minutes}m and ${seconds}s`)
+}
