afe212aa506fd3f4166cb1f62c7a2d77f8c3af88
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-05-19 18:02:01 -0400

    Finish info.js - Complete

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,7 +1,21 @@
+const Discord = require('discord.js')
+
 exports.config = {
 
 }
 
 exports.run = async (client, message) => {
-
+    const uptime = require('../resources/modules/uptime.js')(client)
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Bot Information')
+    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    .setFooter(client.user.username)
+    .setTimestamp(new Date())
+    .setColor('67c6fa')
+    .setDescription(`Running Version: **${require('../../package.json').version}**`)
+    .setThumbnail(client.user.displayAvatarURL())
+    .addField('Uptime', uptime, true)
+    .addField('Servers', `Running in ${client.guilds.cache.size} guilds.`, true)
+    .addField('Useful Links', `**[Website](https://slappyball.com) | [Invite]() | [Slappyball Discord](https://discord.gg/mzrCpw7ttZ) | [Github](https://github.com/F1sxher/Slappybot)**`)
+    message.lineReplyNoMention(embed)
 }
