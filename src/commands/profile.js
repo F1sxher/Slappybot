44374211532c72fe7b928ad92bd0c1501a4fd29f
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-05-21 11:48:34 -0400

    Create profile.js - Initial Commit

---
+++ src/commands/profile.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+
+exports.config = {
+    'name': 'profile',
+    'description': 'Displays in-game profile information.',
+    'usage': 'profile (user)',
+    'aliases': ['getprofile', 'whois'],
+    'alias_str': 'getprofile, whois',
+    'category': 'Miscellaneous'
+}
+
+exports.run = async (client, message, args) => {
+
+}
