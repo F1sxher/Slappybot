c486931e263907aa8e44588625f0c10498630abc
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-05-21 20:33:46 -0400

    Create profile.js - Initial Commit & Start

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+let fetch = require('node-fetch')
 
 exports.config = {
     'name': 'profile',
@@ -10,5 +11,38 @@ exports.config = {
 }
 
 exports.run = async (client, message, args) => {
+    let headers = {
+        "x-api-key": process.env.API_KEY,
+        "PlayerId": [args[0]]
+    }
+    fetch(process.env.API_URI, { method: 'GET', headers: headers})
+    .then((res) => {
+       return res.json()
+    })
+    .then((json) => {
+    let plrData = json;
 
+    if(plrData.error){
+        return message.lineReplyNoMention(`An error has occured: ${plrData.error}`)
+    }
+    plrData = plrData.playerData[0]; 
+    let wlRatio = (plrData.Wins.S / plrData.Losses.N);
+    
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Bot Information')
+    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    .setFooter(client.user.username)
+    .setTimestamp(new Date())
+    .setColor('67c6fa')
+    .setDescription(`Showing Player Data for Steam ID: ${plrData.Id.S}\n**This command is a work in progress, expect changes**`)
+    .addField('Current Team', plrData.CurrentTeam.S)
+    .addField('Level', plrData.PlayerLevel.S, true)
+    .addField('Points', plrData.Points.S, true)
+    .addField('Lifetime Coins', plrData.LifetimeCoins.S)
+    .addField('Wins', plrData.Wins.N, true)
+    .addField('Losses', plrData.Losses.N, true)
+    .addField('Win/Lose Ratio',wlRatio , true)
+
+    message.lineReplyNoMention(embed)
+    });
 }
