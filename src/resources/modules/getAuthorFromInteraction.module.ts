a8fe7653dfd5c58c3d9594fb042c9cf02c011e26
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-06-05 10:50:40 -0400

    Update getAuthor...raction.js - Transition to TS module

--- src/resources/modules/getAuthorFromInteraction.module.ts
+++ src/resources/modules/getAuthorFromInteraction.module.ts
@@ -1,5 +1,5 @@
-module.exports = function(interaction){
-    let authorTag = `${interaction.member.user.username}#${interaction.member.user.discriminator}`
+module.exports = function(interaction: any){
+    let authorTag: string = `${interaction.member.user.username}#${interaction.member.user.discriminator}`
     let authorAvatar = `https://cdn.discordapp.com/avatars/${interaction.member.user.id}/${interaction.member.user.avatar}.png`
     return {"tag": authorTag, "avatar_url": authorAvatar}
 }
